1f489053bfac9b0f351d8f4c12f6dcd089043748
M.Kariuki <kariukimac2005@gmail.com>
2023-02-16 10:24:12 +0300

    finished game

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 # This program demonstrates a guessing game
-from random import  randint
+from random import randint
 
 # 1. Get user input
 user_name = input('Input Name: ')
@@ -7,14 +7,24 @@ print('Hello there ' + user_name)
 
 # 2.Using a while loop
 # 3.Get user number
-number = randint(10, 50)
+# 4.Generate a random number
+random_number = randint(10, 50)
 
 counter = 0
 while counter < 5:
     user_number = eval(input("Give me a number: "))
+    counter += 1
 
+    if user_number < random_number:
+        print("HA! Too low!!")
+    elif user_number > random_number:
+        print("HE..HE..HE! Too high Fool!!")
+    elif user_number == random_number:
+        break
 
-
-
-# 4.Generate a random number
+if user_number == random_number:
+    print("Ok,FINE! You win")
+else:
+    print("YOU DUMB BRO! you lose")
+    print("The answer is " + str(random_number))
 # Check if user input is equal to generated number
